e552981513f775d070fcb604807c83c461e3e4b5
JVGFern <50374847+JVGFern@users.noreply.github.com>
2020-04-16 19:08:13 -0300

    Initial commit

---
+++ src/routes/transaction.routes.ts
@@ -0,0 +1,38 @@
+import { Router } from 'express';
+import TransactionsRepository from '../repositories/TransactionsRepository';
+import CreateTransactionService from '../services/CreateTransactionService';
+
+const transactionRouter = Router();
+
+const transactionsRepository = new TransactionsRepository();
+const createTransactionService = new CreateTransactionService(
+  transactionsRepository,
+);
+
+transactionRouter.get('/', (req, res) => {
+  try {
+    const transactions = transactionsRepository.all();
+    const balance = transactionsRepository.getBalance();
+    return res.json({ transactions, balance });
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+transactionRouter.post('/', (req, res) => {
+  try {
+    const { title, value, type } = req.body;
+
+    const transaction = createTransactionService.execute({
+      title,
+      value,
+      type,
+    });
+
+    return res.json(transaction);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+export default transactionRouter;
